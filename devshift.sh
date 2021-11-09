4084e0700cc76d89e5457b05e69d2125fece5edd
manojawsdevops <53854186+manojawsdevops@users.noreply.github.com>
2021-11-09 18:24:27 +0530

    Update devshift.sh

--- devshift.sh
+++ devshift.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-/usr/local/bin/oc login -u kubeadmin -p r6PzC-4hayC-TjCIg-NaL4m --insecure-skip-tls-verify=true
+/usr/local/bin/oc login -u kubeadmin -p pNEYN-4x6FF-WhdXS-gS3t4 --insecure-skip-tls-verify=true
 x=devopsimage
 y=$(/usr/local/bin/oc get routes | awk '{print $1}' | grep devopsimage)
 if [ "$x" == "$y" ]
 then
     echo "Pod and services already existed in openshift"
 else
-    /usr/local/bin/oc new-app --image-stream=manoj/devopsimage:v1 -e LICENSE=accept && /usr/local/bin/oc create route passthrough devopsimage --service=devopsimage  --port=7843
+    /usr/local/bin/oc new-app --image-stream=manoj1/devopsimage:v1 -e LICENSE=accept && /usr/local/bin/oc expose svc devopsimage --port=7800
 fi
